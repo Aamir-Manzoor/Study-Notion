28ceabf70a4cf49647b05189ecf39374299c140b
aamir221 <aamirmanzoor372@gmail.com>
2023-11-01 01:29:04 +0530

    Styled Verify Email Page

--- src/Pages/VerifyEmail.jsx
+++ src/Pages/VerifyEmail.jsx
@@ -3,6 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 import OTPInput from "react-otp-input";
 import { useSelector, useDispatch } from "react-redux";
 import { sendOtp, signUp } from "../services/operations/authAPI";
+import {BiArrowBack} from "react-icons/bi"
+import { RxCountdownTimer} from "react-icons/rx"
 
 const VerifyEmail = () => {
   const { signupData, loading } = useSelector((state) => state.auth);
@@ -16,7 +18,7 @@ const VerifyEmail = () => {
     }
   }, [navigate, signupData]);
 
-  const handleOnSubmit = (e) => {
+  const handleVerifyAndSignup = (e) => {
     e.preventDefault();
     const {
       accountType,
@@ -41,34 +43,59 @@ const VerifyEmail = () => {
     );
   };
   return (
-    <div className="text-white">
+    <div className="min-h-[calc(100vh-3.5rem)] grid place-items-center">
       {loading ? (
-        <div>Loading...</div>
+        <div className="spinner"></div>
       ) : (
-        <div>
-          <h1>Verify Email</h1>
-          <p>A verification code has been sent to you. Enter the code below</p>
-          <form onSubmit={handleOnSubmit}>
+        <div className="max-w-[31.3rem] p-4 lg:p-8">
+          <h1 className="text-richblack-5 font-semibold text-[1.875rem] leading-[2.375rem]">
+            Verify Email
+          </h1>
+          <p className="text-[1.125rem] leading-[1.625rem] my-4 text-richblack-100">
+            A verification code has been sent to you. Enter the code below
+          </p>
+          <form onSubmit={handleVerifyAndSignup}>
             <OTPInput
               value={otp}
               onChange={setOtp}
               numInputs={6}
               renderSeparator={<span>-</span>}
               renderInput={(props) => (
-                <input {...props} className="bg-richblack-800" />
+                <input
+                  {...props}
+                  placeholder="-"
+                  style={{
+                    boxShadow: "inset 0px -1px 0px rgba(255,255,255, 0.18)",
+                  }}
+                  className="w-[3rem] lg:w-[3.8rem] border-0 bg-richblack-800 rounded-[0.5rem] text-richblack-5 aspect-square text-center
+                focus:border-0 focus:outline-2 focus:outline-yellow-50"
+                />
               )}
+              containerStyle={{
+                justifyContent: "space-between",
+                gap: "0 6px",
+              }}
             />
 
-            <button type="submit">Verify Email</button>
+            <button
+              className="w-full bg-yellow-50 py-[0.8rem] px-[0.8rem] rouned-[0.5rem] mt-6 font-medium text-richblack-900"
+              type="submit"
+            >
+              Verify Email
+            </button>
           </form>
-          <div>
-            <div>
-              <Link to="/login">
-                <p> Back to Login </p>
+          <div className="mt-6 flex items-center justify-between">
+    
+              <Link to="/signup"  >
+            
+                <p className="text-richblack-5 flex items-center gap-x-2">   <BiArrowBack/>  Back to Signup </p>
               </Link>
-            </div>
 
-            <button onClick={() => dispatch(sendOtp(signupData.email))}>
+            <button
+            className="flex items-center text-blue-100 gap-x-2"
+            
+            onClick={() => dispatch(sendOtp(signupData.email, navigate))}>
+            <RxCountdownTimer />
               Resend it
             </button>
           </div>
