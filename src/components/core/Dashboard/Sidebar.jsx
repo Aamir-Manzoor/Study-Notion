90e418aeee4689cdbf71fbb316338ec6fa25097c
aamir221 <aamirmanzoor372@gmail.com>
2023-11-02 02:00:28 +0530

    Sidebar Added without modal

--- src/components/core/Dashboard/Sidebar.jsx
+++ src/components/core/Dashboard/Sidebar.jsx
@@ -1,37 +1,55 @@
-import React from 'react'
-import { sidebarLinks } from '../../../data/dashboard-links'
-import {logout} from '../../../services/operations/authAPI'
-import { useSelector } from 'react-redux'
-import SidebarLink from './SidebarLink' 
+import React from "react";
+import { sidebarLinks } from "../../../data/dashboard-links";
+import { logout } from "../../../services/operations/authAPI";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import SidebarLink from "./SidebarLink";
+import { VscSettingsGear, VscSignOut } from "react-icons/vsc";
 
 const Sidebar = () => {
-    const { user , loading: profileLoading} = useSelector( (state) => state.profile)
-    const {loading: authLoading} = useSelector( (state) => state.auth)
+  const { user, loading: profileLoading } = useSelector(
+    (state) => state.profile
+  );
+  const { loading: authLoading } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    if(profileLoading || authLoading){
-        return (
-            <div className='spinner grid place-items-center'>
-            
-            </div>
-        )
-    }
+  if (profileLoading || authLoading) {
+    return (
+      <div className="grid h-[calc(100vh-3.5rem)] min-w-[15rem] items-center border-r-[1px] border-r-richblack-700 bg-richblack-800">
+        <div className="spinner"></div>;
+      </div>
+    );
+  }
   return (
-    <div>
-      <div>
-      <div>
-      {
-        sidebarLinks.map( (link, index) => {
-          if(link.type && user?.accountType !== link.type) return null;
+    <>
+      <div className="hidden lg:flex h-[calc(100vh-3.5rem)] min-w-[15rem] flex-col border-r-richblack-700 bg-richblack-800 py-10">
+        <div className="lex flex-col">
+            {sidebarLinks.map((link, index) => {
+              if (link.type && user?.accountType !== link.type) return null;
 
-          return (
-            <SidebarLink/>
-          )
-        })
-      }
-      </div>
-      </div>
-    </div>
-  )
-}
+              return (<SidebarLink key={link.id} link={link} iconName={link.icon}  />);
+            })}
+        </div>
+        <div className="mx-auto mt-6 mb-6 h-[1px] w-10/12 bg-richblack-700"/>
+        <div className="flex flex-col">
+        <SidebarLink
+        link={{name: "settings", path:"/dashboard/settings"}}
+        iconName="VscSettingsGear"
+        />
 
-export default Sidebar
+        <button
+        className="px-8 py-2 ttext-sm font-medium text-richblack-300"
+        >
+        <div className="flex items-center gap-x-2">
+        <VscSignOut className="text-lg"/>
+        <span>logout</span>
+        </div>
+        </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
