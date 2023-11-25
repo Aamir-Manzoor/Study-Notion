534e659e46cd987b242dcfa62dc16d45ce7c9e15
aamir221 <aamirmanzoor372@gmail.com>
2023-11-25 22:58:37 +0530

    added

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Error = () => {
   return (
-    <div className='flex place-items-center text-3xl text-white'>
+    <div className='flex place-items-center mx-auto text-3xl text-white'>
       Error - 404
     </div>
   )
