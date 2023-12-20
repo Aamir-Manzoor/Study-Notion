93d0d820bd0a174372c8bd26aead65572980c134
aamir221 <aamirmanzoor372@gmail.com>
2023-12-20 20:51:58 +0530

    fixing

--- src/services/operations/studentFeaturesAPI.js
+++ src/services/operations/studentFeaturesAPI.js
@@ -29,7 +29,7 @@ export async function buyCourse(token, courses, userDetails, navigate, dispatch)
     const toastId = toast.loading("Loading...");
     try{
         //load the script
-        console.log("ye chl gya")
+        
         const res = await loadScript("https://checkout.razorpay.com/v1/checkout.js");
 
         if(!res) {
